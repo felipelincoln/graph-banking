0aa693661b0a01d5aad840cedf4279bf3dff972a
Felipe Lincoln <felipelincoln@usp.br>
2020-11-19 07:36:56 -0300

    install credo

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule GraphBanking.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
