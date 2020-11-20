55cc1fdce7dfa4cbebe0e0227216a52c25d12f61
Felipe Lincoln <felipelincoln@usp.br>
2020-11-20 11:32:13 -0300

    implement public api

--- lib/graph_banking.ex
+++ lib/graph_banking.ex
@@ -6,4 +6,41 @@ defmodule GraphBanking do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias GraphBanking.{Account, Repo, Transaction}
+
+  def get_account!(uuid) do
+    Account
+    |> Repo.get!(uuid)
+    |> Repo.preload([:transactions_in, :transactions_out])
+  end
+
+  def new_account(params \\ %{}) do
+    %Account{}
+    |> Account.changeset(params)
+    |> Repo.insert()
+  end
+
+  def update_account!(%Account{} = account, params \\ %{}) do
+    account
+    |> Account.changeset(params)
+    |> Repo.update!()
+  end
+
+  def new_transaction!(params \\ %{}) do
+    %Transaction{}
+    |> Transaction.changeset(params)
+    |> Repo.insert!()
+  end
+
+  def transfer_money(sender_uuid, address_uuid, amount) do
+    Repo.transaction(fn ->
+      sender = get_account!(sender_uuid)
+      address = get_account!(address_uuid)
+
+      new_transaction!(%{sender_uuid: sender_uuid, address_uuid: address_uuid, amount: amount})
+      update_account!(sender, %{current_balance: sender.current_balance - amount})
+      update_account!(address, %{current_balance: address.current_balance + amount})
+    end)
+  end
 end
