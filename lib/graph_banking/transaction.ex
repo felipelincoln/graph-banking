419ced8dcda0a0ff0160bcdba254f78f32cdc8d2
Felipe Lincoln <felipelincoln@usp.br>
2020-11-20 08:36:15 -0300

    implemenent autogenerated date

--- lib/graph_banking/transaction.ex
+++ lib/graph_banking/transaction.ex
@@ -3,12 +3,14 @@ defmodule GraphBanking.Transaction do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key {:uuid, :binary_id, autogenerate: true}
+  @timestamps_opts [inserted_at: :when, updated_at: false, type: :utc_datetime]
+  @primary_key {:uuid, :binary_id, [autogenerate: true]}
   @foreign_key_type :binary_id
 
   schema "transactions" do
     field :amount, :float
-    field :when, :utc_datetime
+
+    timestamps()
 
     belongs_to :sender, GraphBanking.Account, references: :uuid, foreign_key: :sender_uuid
     belongs_to :address, GraphBanking.Account, references: :uuid, foreign_key: :address_uuid
@@ -16,8 +18,8 @@ defmodule GraphBanking.Transaction do
 
   def changeset(%GraphBanking.Transaction{} = trans, attrs) do
     trans
-    |> cast(attrs, [:amount, :when, :sender_uuid, :address_uuid])
-    |> validate_required([:amount, :when, :sender_uuid, :address_uuid])
+    |> cast(attrs, [:amount, :sender_uuid, :address_uuid])
+    |> validate_required([:amount, :sender_uuid, :address_uuid])
     |> assoc_constraint(:sender)
     |> assoc_constraint(:address)
   end
