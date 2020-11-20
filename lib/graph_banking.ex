db99560abb7f912db59129697f8adbdb7ef49868
Felipe Lincoln <felipelincoln@usp.br>
2020-11-20 11:44:47 -0300

    handle transactions errors

--- lib/graph_banking.ex
+++ lib/graph_banking.ex
@@ -34,13 +34,22 @@ defmodule GraphBanking do
   end
 
   def transfer_money(sender_uuid, address_uuid, amount) do
-    Repo.transaction(fn ->
-      sender = get_account!(sender_uuid)
-      address = get_account!(address_uuid)
+    try do
+      Repo.transaction(fn ->
+        operation(sender_uuid, address_uuid, amount)
+      end)
+    rescue
+      error -> error
+    end
+  end
 
-      new_transaction!(%{sender_uuid: sender_uuid, address_uuid: address_uuid, amount: amount})
-      update_account!(sender, %{current_balance: sender.current_balance - amount})
-      update_account!(address, %{current_balance: address.current_balance + amount})
-    end)
+  defp operation(sender_uuid, address_uuid, amount) do
+    new_transaction!(%{sender_uuid: sender_uuid, address_uuid: address_uuid, amount: amount})
+
+    sender = get_account!(sender_uuid)
+    address = get_account!(address_uuid)
+
+    update_account!(sender, %{current_balance: sender.current_balance - amount})
+    update_account!(address, %{current_balance: address.current_balance + amount})
   end
 end
