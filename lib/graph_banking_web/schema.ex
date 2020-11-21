853f34262d97f9008d8c7185adad634f76d1b226
Felipe Lincoln <felipelincoln@usp.br>
2020-11-21 08:21:30 -0300

    create GraphiQL interface documentation

--- lib/graph_banking_web/schema.ex
+++ lib/graph_banking_web/schema.ex
@@ -3,34 +3,52 @@ defmodule GraphBankingWeb.Schema do
   use Absinthe.Schema
   alias GraphBankingWeb.Resolver
 
+  @desc """
+  Transactions are currency operations between two accounts
+  and it doens't keep the sender identifier.
+  It is created by the `transferMoney` mutation.
+  """
   object :transaction do
-    field :uuid, :string
-    field :address_uuid, :string, name: "address"
-    field :amount, :float
-    field :when, :string, resolve: &shift_timezone/3
+    field :uuid, :string, description: "The transaction's unique identifier."
+    field :amount, :float, description: "The amount of money sent."
+
+    field :address_uuid, :string,
+      name: "address",
+      description: "The account that received the money."
+
+    field :when, :string,
+      resolve: &shift_timezone/3,
+      description: "When the operation took place."
   end
 
-  defp shift_timezone(trans, _, _), do: {:ok, DateTime.add(trans.when, -10_800)}
-
+  @desc "Accounts are created by the `openAccount` mutation."
   object :account do
-    field :uuid, :string
-    field :current_balance, :float
-    field :transactions_out, list_of(:transaction), name: "transactions"
+    field :uuid, :string, description: "The account's unique identifier."
+    field :current_balance, :float, description: "The account's current balance."
+
+    field :transactions_out, list_of(:transaction),
+      name: "transactions",
+      description: "All the transactions made."
   end
 
+  @desc "The query root of GraphBanking's GraphQL interface."
   query do
+    @desc "Gets a single account."
     field :account, :account do
       arg(:uuid, non_null(:string))
       resolve(&Resolver.get_account/2)
     end
   end
 
+  @desc "The root query for implementing GraphQL mutations."
   mutation do
+    @desc "Creates a new account."
     field :open_account, :account do
       arg(:current_balance, :float)
       resolve(&Resolver.new_account/2)
     end
 
+    @desc "Transfer `amount` from `sender` to `address`."
     field :transfer_money, :transaction do
       arg(:sender, :string)
       arg(:address, :string)
@@ -38,4 +56,6 @@ defmodule GraphBankingWeb.Schema do
       resolve(&Resolver.transfer_money/2)
     end
   end
+
+  defp shift_timezone(trans, _, _), do: {:ok, DateTime.add(trans.when, -10_800)}
 end
