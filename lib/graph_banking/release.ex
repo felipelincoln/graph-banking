7baec7fdf5903a5590f27341693edf9614fb5224
Felipe Lincoln <felipelincoln@usp.br>
2020-11-20 21:27:54 -0300

    add migrator script

---
+++ lib/graph_banking/release.ex
@@ -0,0 +1,27 @@
+defmodule GraphBanking.Release do
+  @moduledoc false
+
+  @app :graph_banking
+
+  # bin/graph_banking eval "GraphBanking.Release.migrate"
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
+  end
+end
