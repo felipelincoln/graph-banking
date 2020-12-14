4767b182c5684e2a1b54c5c3190124c70fac76a0
Felipe Lincoln <felipelincoln@usp.br>
2020-12-14 16:06:35 -0300

    remove tests

--- test/graph_banking_web/resolver_test.exs
+++ test/graph_banking_web/resolver_test.exs
@@ -19,31 +19,4 @@ defmodule GraphBankingWeb.ResolverTest do
     uuid = Ecto.UUID.generate()
     assert {:error, _err_msg} = Resolver.get_account(%{uuid: uuid}, %{})
   end
-
-  test "new_account/2 with a positive balance returns the new account" do
-    assert {:ok, %Account{}} = Resolver.new_account(%{current_balance: 10}, %{})
-  end
-
-  test "new_account/2 with a negative balance returns error" do
-    assert {:error, _err_msg} = Resolver.new_account(%{current_balance: -10}, %{})
-  end
-
-  test "transfer_money/2 with valid data returns the new transaction" do
-    sender = account_fixture(45.5)
-    address = account_fixture(10)
-
-    params = %{sender: sender.uuid, address: address.uuid, amount: 15.50}
-    {:ok, %Transaction{}} = Resolver.transfer_money(params, %{})
-  end
-
-  test "transfer_money/2 with invalid data returns error" do
-    sender = account_fixture(45.5)
-    address = account_fixture(10)
-
-    params = %{sender: sender.uuid, address: address.uuid, amount: 115.50}
-    assert {:error, _err_msg} = Resolver.transfer_money(params, %{})
-
-    params = %{sender: sender.uuid, address: address.uuid, amount: -115.50}
-    assert {:error, _err_msg} = Resolver.transfer_money(params, %{})
-  end
 end
